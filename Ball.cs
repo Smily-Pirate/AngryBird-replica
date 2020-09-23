e25dcd3b3d5d6247c0c71cc93f5f65625046410d
Ghanshyam Joshi <47424158+Ghansu@users.noreply.github.com>
2020-09-23 06:50:25 -0700

    Practice Unity and C#

---
+++ Ball.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Ball : MonoBehaviour
+{
+    private bool isPressed = false;
+
+    public float releaseTime = 0.1f;
+
+    public Rigidbody2D hook;
+
+    public float maxDistance = 2f;
+
+    public Rigidbody2D rb;
+
+    public GameObject nextBall;
+
+    void Update()
+    {
+        if (isPressed)
+        {
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            if (Vector3.Distance(mousePos, hook.position) > maxDistance)
+            {
+                rb.position = hook.position + (mousePos - hook.position).normalized * maxDistance;
+            }
+            else
+            {
+                rb.position = mousePos;
+            }
+        }
+    }
+
+    void OnMouseDown ()
+    {
+        isPressed = true;
+        rb.isKinematic = true;
+    }
+
+    void OnMouseUp ()
+    {
+        isPressed = false;
+        rb.isKinematic = false;
+
+        StartCoroutine(Release());
+    }
+
+    IEnumerator Release ()
+    {
+        yield return new WaitForSeconds(releaseTime);
+
+        GetComponent<SpringJoint2D>().enabled = false;
+        this.enabled = false;
+
+        yield return new WaitForSeconds(2f);
+
+        if (nextBall != null)
+        {
+            nextBall.SetActive(true);
+        }
+        else
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
---
+++ Enemy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float health = 4f;
+
+    public GameObject deathEffect;
+
+    void OnCollisionEnter2D (Collision2D colinfo)
+    {
+        if (colinfo.relativeVelocity.magnitude > health)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Instantiate(deathEffect, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+}
